a69265a9bc0390a9bbb00987726b6ae42dfcd7d3
joshua-stumpf55 <153780390+joshua-stumpf55@users.noreply.github.com>
2024-10-01 19:50:57 -0400

    Add files via upload

---
+++ src/SnakeGame.java
@@ -0,0 +1,10 @@
+public class SnakeGame {
+
+    public static void main(String[] args) {
+        new GameFrame();
+    }
+
+}
+
+// this project will also consist of a main menu as well as additional on screen options. This will be added during the week of module 7-
+// to ensure that I meet all requirements for this project.
